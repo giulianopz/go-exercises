05efc1e0d7812123ca3cca42e9981781859a82fc
giuliano <panzironi.giuliano@gmail.com>
2022-06-23 23:54:35 +0200

    update

--- helper/data/mysql.sh
+++ helper/data/mysql.sh
@@ -2,3 +2,6 @@
 
 podman run -d --name=mysql -e MYSQL_ROOT_PASSWORD=pwd -v $(pwd):/opt/db:Z -p=3306:3306 mysql
 podman exec -it mysql /bin/sh -c "mysql -u root -ppwd </opt/db/initdb.sql"
+
+export DBUSER=root
+export DBPASS=pwd
