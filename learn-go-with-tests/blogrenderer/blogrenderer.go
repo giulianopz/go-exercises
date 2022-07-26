e8966e8b38c0e14217a487b1046a6f222b735a66
giuliano <panzironi.giuliano@gmail.com>
2022-07-26 23:58:07 +0200

    add first half of rendering templates chapter

---
+++ learn-go-with-tests/blogrenderer/blogrenderer.go
@@ -0,0 +1,40 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+
+	if err := r.templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// if you're continuing from the read files chapter, you shouldn't redefine this
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
